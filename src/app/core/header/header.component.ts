45abd8cc651c970c49be82e42e09abe9695282cf
Magnus <bcict20070@ttu.edu.gh>
2025-08-11 16:51:53 +0000

    feat: enhance HeaderComponent with navigation links, search input, and icons

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { SubHeaderComponent } from '../sub-header/sub-header.component';
+import { RouterLink } from '@angular/router';
+import { LucideAngularModule, Search, Heart, ShoppingCart } from 'lucide-angular';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [SubHeaderComponent, RouterLink, LucideAngularModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+
+  readonly icons = {
+    Search, Heart, ShoppingCart
+   };
+
+  navLinks = [
+    { label: 'Home', link: '/home' },
+    { label: 'Contact', link: '/contact' },
+    { label: 'About', link: '/about' },
+    { label: 'Sign Up', link: '/signup' }
+  ]
 
 }
