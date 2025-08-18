a41d071357aeed897130c83f479ba55635ecc22e
Magnus <bcict20070@ttu.edu.gh>
2025-08-18 12:15:25 +0000

    feat: add FlashsalesComponent with HTML, SCSS, and unit tests

---
+++ src/app/features/flashsales/flashsales.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-flashsales',
+  imports: [],
+  templateUrl: './flashsales.component.html',
+  styleUrl: './flashsales.component.scss'
+})
+export class FlashsalesComponent {
+
+}
