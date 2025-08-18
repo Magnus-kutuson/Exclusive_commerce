3c13d55ac1b5c8ffa7bbb5999d764064749019b4
Magnus <bcict20070@ttu.edu.gh>
2025-08-18 12:16:27 +0000

    refactor: remove SCSS file and fix styleUrl in FlashsalesComponent

--- src/app/features/flashsales/flashsales.component.ts
+++ src/app/features/flashsales/flashsales.component.ts
@@ -4,7 +4,6 @@ import { Component } from '@angular/core';
   selector: 'app-flashsales',
   imports: [],
   templateUrl: './flashsales.component.html',
-  styleUrl: './flashsales.component.scss'
 })
 export class FlashsalesComponent {
 
