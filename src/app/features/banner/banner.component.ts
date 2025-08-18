4e081c853ea1cfc393efad3a1764852dc8bcca96
Magnus <bcict20070@ttu.edu.gh>
2025-08-18 17:26:27 +0000

    feat: integrate BannerComponent into AppComponent and remove unused styles

--- src/app/features/banner/banner.component.ts
+++ src/app/features/banner/banner.component.ts
@@ -4,7 +4,6 @@ import { Component } from '@angular/core';
   selector: 'app-banner',
   imports: [],
   templateUrl: './banner.component.html',
-  styleUrl: './banner.component.scss'
 })
 export class BannerComponent {
 
