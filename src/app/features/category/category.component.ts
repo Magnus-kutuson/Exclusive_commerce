502f66cf75f6db8cd5dc13928e119b46deb50c05
Magnus <bcict20070@ttu.edu.gh>
2025-08-18 15:26:51 +0000

    feat: enhance CategoryComponent with new layout, navigation controls, and category icons

--- src/app/features/category/category.component.ts
+++ src/app/features/category/category.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
+import { LucideAngularModule, Smartphone, Computer, Watch, Camera, Headphones, Gamepad, ChevronLeft, ChevronRight } from 'lucide-angular';
 
 @Component({
   selector: 'app-category',
-  imports: [],
+  imports: [LucideAngularModule],
   templateUrl: './category.component.html',
 })
 export class CategoryComponent {
+  icons: any;
+  next() {
+    throw new Error('Method not implemented.');
+  }
+  prev() {
+    throw new Error('Method not implemented.');
+  }
+  categoryIcon = {
+    Smartphone,
+    Computer,
+    Watch,
+    Camera,
+    Headphones,
+    Gamepad,
+    ChevronLeft,
+    ChevronRight
+  };
 
+  categories = [
+    { label: 'Smartphone', icon: this.categoryIcon.Smartphone },
+    { label: 'Computer', icon: this.categoryIcon.Computer },
+    { label: 'Watch', icon: this.categoryIcon.Watch },
+    { label: 'Camera', icon: this.categoryIcon.Camera },
+    { label: 'Headphones', icon: this.categoryIcon.Headphones },
+    { label: 'Gamepad', icon: this.categoryIcon.Gamepad },
+  ];
 }
