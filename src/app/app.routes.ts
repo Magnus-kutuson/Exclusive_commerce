c1bffbe5f1d881baa929e2a0fbb04e09c1c17438
Magnus <bcict20070@ttu.edu.gh>
2025-08-19 12:55:43 +0000

    feat: add routing for home and signup components to improve navigation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    {
+        path: 'home',
+        loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent)
+    },
+    {
+        path: 'signup',
+        loadComponent: () => import('./pages/auth/signup/signup.component').then(m => m.SignupComponent)
+    }
+];
