b4d11b9c566dccd7e730dbda23f03c482c506cba
Magnus <bcict20070@ttu.edu.gh>
2025-08-19 14:45:35 +0000

    feat: add login route to enable user authentication flow

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,5 +9,9 @@ export const routes: Routes = [
     {
         path: 'signup',
         loadComponent: () => import('./pages/auth/signup/signup.component').then(m => m.SignupComponent)
+    },
+    {
+        path: 'login',
+        loadComponent: () => import('./pages/auth/login/login.component').then(m => m.LoginComponent)
     }
 ];
