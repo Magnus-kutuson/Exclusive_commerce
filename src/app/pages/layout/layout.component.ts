46c26bef2bf8b47ac7a058f3bfd43c382a1ea75f
Magnus <bcict20070@ttu.edu.gh>
2025-08-19 15:35:05 +0000

    feat: update LayoutComponent to include Header and Footer components, enhancing overall layout structure

--- src/app/pages/layout/layout.component.ts
+++ src/app/pages/layout/layout.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
-
+import { FooterComponent } from "../../core/footer/footer.component";
+import { HeaderComponent } from "../../core/header/header.component";
+import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-layout',
-  imports: [],
+  imports: [FooterComponent, HeaderComponent, RouterOutlet],
   templateUrl: './layout.component.html',
 })
 export class LayoutComponent {
