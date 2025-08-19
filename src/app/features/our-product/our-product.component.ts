1e0da27f57f2f52a47846356f984d6cce14693f9
Magnus <bcict20070@ttu.edu.gh>
2025-08-19 08:39:42 +0000

    feat: enhance OurProductComponent with navigation controls and updated layout

--- src/app/features/our-product/our-product.component.ts
+++ src/app/features/our-product/our-product.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { ChevronLeft, ChevronRight, LucideAngularModule } from "lucide-angular";
 
 @Component({
   selector: 'app-our-product',
-  imports: [],
+  imports: [LucideAngularModule],
   templateUrl: './our-product.component.html',
 })
 export class OurProductComponent {
-
+  next() {
+    throw new Error('Method not implemented.');
+  }
+  prev() {
+    throw new Error('Method not implemented.');
+  }
+  categoryIcon = {
+   
+    ChevronLeft,
+    ChevronRight,
+   
+  };
 }
