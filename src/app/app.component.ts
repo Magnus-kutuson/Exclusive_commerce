6934ff8a3ab44c57d4b57bb4cba7b04d41bdef05
Magnus <bcict20070@ttu.edu.gh>
2025-08-19 12:55:26 +0000

    refactor: remove unused components from AppComponent and clean up template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './core/header/header.component';
-import { FooterComponent } from './core/footer/footer.component';
-import { HeroSectionComponent } from './features/hero-section/hero-section.component';
-import { FlashsalesComponent } from './features/flashsales/flashsales.component';
-import { CategoryComponent } from "./features/category/category.component";
-import { ProductComponent } from './features/product/product.component';
-import { BannerComponent } from './features/banner/banner.component';
-import { OurProductComponent } from './features/our-product/our-product.component';
-import { NewArrivalsComponent } from "./features/new-arrivals/new-arrivals.component";
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, HeroSectionComponent, FlashsalesComponent, CategoryComponent, ProductComponent, BannerComponent, OurProductComponent, NewArrivalsComponent],
+  imports: [RouterOutlet ],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
