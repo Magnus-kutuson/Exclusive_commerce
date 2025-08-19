4836a51b6dfa9c681bfd217c2977a266004e7f55
Magnus <bcict20070@ttu.edu.gh>
2025-08-19 12:15:33 +0000

    feat: enhance NewArrivalsComponent with feature icons and descriptions for improved user engagement

--- src/app/features/new-arrivals/new-arrivals.component.ts
+++ src/app/features/new-arrivals/new-arrivals.component.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
+import { Headphones, LucideAngularModule, ShieldCheck, Truck } from 'lucide-angular';
 
 @Component({
   selector: 'app-new-arrivals',
-  imports: [],
+  imports: [LucideAngularModule],
   templateUrl: './new-arrivals.component.html',
-  styleUrl: './new-arrivals.component.scss'
+  styleUrl: './new-arrivals.component.scss',
 })
 export class NewArrivalsComponent {
+  icons = { Truck, Headphones, ShieldCheck };
 
+  features = [
+    {
+      icon: this.icons.Truck,
+      title: 'FREE AND FAST DELIVERY',
+      description: 'Free delivery for all orders over $140',
+    },
+    {
+      icon: this.icons.Headphones,
+      title: '24/7 CUSTOMER SERVICE',
+      description: 'Friendly 24/7 customer support',
+    },
+    {
+      icon: this.icons.ShieldCheck,
+      title: 'MONEY BACK GUARANTEE',
+      description: 'We return money within 30 days',
+    },
+  ];
 }
