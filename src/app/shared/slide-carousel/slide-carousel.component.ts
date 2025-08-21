8b29b58b4b7fc3fabb18f881b7bf63252ec2003c
Magnus <bcict20070@ttu.edu.gh>
2025-08-21 09:57:28 +0000

    feat: add SlideCarouselComponent with template, styles, and unit tests for carousel functionality

---
+++ src/app/shared/slide-carousel/slide-carousel.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-slide-carousel',
+  imports: [],
+  templateUrl: './slide-carousel.component.html',
+  styleUrl: './slide-carousel.component.scss'
+})
+export class SlideCarouselComponent {
+
+}
