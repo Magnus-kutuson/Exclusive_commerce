77d5781bee1f2bf9f9350f660014e2baac3a0b43
Magnus <bcict20070@ttu.edu.gh>
2025-08-25 11:03:33 +0000

    refactor: reorganize route definitions for improved readability

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,35 +2,49 @@ import { Routes } from '@angular/router';
 import { LayoutComponent } from './pages/layout/layout.component';
 
 export const routes: Routes = [
-    {
-        path: '',
-        component: LayoutComponent,
-        children: [
-
-            { path: '', redirectTo: 'home', pathMatch: 'full' },
-            {
-                path: 'home',
-                loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent)
-            }, {
-                path: 'about',
-                loadComponent: () => import('./pages/about/about.component').then(m => m.AboutComponent)
-            }
-            , {
-                path: 'contact',
-                loadComponent: () => import('./pages/contact/contact.component').then(m => m.ContactComponent)
-            }
-            , {
-                path: '**',
-                loadComponent: () => import('./pages/bot-found/bot-found.component').then(m => m.BotFoundComponent)
-            }
-        ]
-    },
-    {
-        path: 'signup',
-        loadComponent: () => import('./pages/auth/signup/signup.component').then(m => m.SignupComponent)
-    },
-    {
-        path: 'login',
-        loadComponent: () => import('./pages/auth/login/login.component').then(m => m.LoginComponent)
-    }
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      { path: '', redirectTo: 'home', pathMatch: 'full' },
+      {
+        path: 'home',
+        loadComponent: () =>
+          import('./pages/home/home.component').then((m) => m.HomeComponent),
+      },
+      {
+        path: 'about',
+        loadComponent: () =>
+          import('./pages/about/about.component').then((m) => m.AboutComponent),
+      },
+      {
+        path: 'contact',
+        loadComponent: () =>
+          import('./pages/contact/contact.component').then(
+            (m) => m.ContactComponent
+          ),
+      },
+    ],
+  },
+  {
+    path: 'signup',
+    loadComponent: () =>
+      import('./pages/auth/signup/signup.component').then(
+        (m) => m.SignupComponent
+      ),
+  },
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./pages/auth/login/login.component').then(
+        (m) => m.LoginComponent
+      ),
+  },
+  {
+    path: '**',
+    loadComponent: () =>
+      import('./pages/bot-found/bot-found.component').then(
+        (m) => m.BotFoundComponent
+      ),
+  },
 ];
