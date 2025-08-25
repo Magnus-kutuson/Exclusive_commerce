030b6ece11362dca3048d241367d32b80c11ca79
Magnus <bcict20070@ttu.edu.gh>
2025-08-25 08:19:50 +0000

    feat: add wishlist route to application routing configuration

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,31 +2,45 @@ import { Routes } from '@angular/router';
 import { LayoutComponent } from './pages/layout/layout.component';
 
 export const routes: Routes = [
-    {
-        path: '',
-        component: LayoutComponent,
-        children: [
-
-            { path: '', redirectTo: 'home', pathMatch: 'full' },
-            {
-                path: 'home',
-                loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent)
-            }, {
-                path: 'about',
-                loadComponent: () => import('./pages/about/about.component').then(m => m.AboutComponent)
-            }
-            , {
-                path: 'contact',
-                loadComponent: () => import('./pages/contact/contact.component').then(m => m.ContactComponent)
-            }
-        ]
-    },
-    {
-        path: 'signup',
-        loadComponent: () => import('./pages/auth/signup/signup.component').then(m => m.SignupComponent)
-    },
-    {
-        path: 'login',
-        loadComponent: () => import('./pages/auth/login/login.component').then(m => m.LoginComponent)
-    }
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      { path: '', redirectTo: 'home', pathMatch: 'full' },
+      {
+        path: 'home',
+        loadComponent: () =>
+          import('./pages/home/home.component').then((m) => m.HomeComponent),
+      },
+      {
+        path: 'about',
+        loadComponent: () =>
+          import('./pages/about/about.component').then((m) => m.AboutComponent),
+      },
+      {
+        path: 'contact',
+        loadComponent: () =>
+          import('./pages/contact/contact.component').then((m) => m.ContactComponent),
+      },
+      {
+        path: 'wishlist',
+        loadComponent: () =>
+          import('./pages/wishlist/wishlist.component').then((m) => m.WishlistComponent),
+      },
+    ],
+  },
+  {
+    path: 'signup',
+    loadComponent: () =>
+      import('./pages/auth/signup/signup.component').then(
+        (m) => m.SignupComponent
+      ),
+  },
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./pages/auth/login/login.component').then(
+        (m) => m.LoginComponent
+      ),
+  },
 ];
