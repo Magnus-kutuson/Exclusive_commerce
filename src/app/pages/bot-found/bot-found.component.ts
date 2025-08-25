60b8c6c78a2712c069aa7bbba54fa02b11c4b8de
Magnus <bcict20070@ttu.edu.gh>
2025-08-25 10:49:56 +0000

    feat: implement 404 error page with breadcrumb navigation

--- src/app/pages/bot-found/bot-found.component.ts
+++ src/app/pages/bot-found/bot-found.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Breadcrumb } from "primeng/breadcrumb";
 
 @Component({
   selector: 'app-bot-found',
-  imports: [],
+  imports: [Breadcrumb, RouterLinkActive, RouterLink],
   templateUrl: './bot-found.component.html',
 })
 export class BotFoundComponent {
 
+  home = { label: 'Home', routerLink: '/home' };
+
+  items = [{ label: '404 Error', routerLink: '**' }];
 }
